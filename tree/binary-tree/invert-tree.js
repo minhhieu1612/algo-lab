6e8707eafe9bbd8e3abf3e75434bea4955b1dd2d
minhhieu1612 <hieunguyenminh562@gmail.com>
2025-12-22 16:53:57 +0700

    resolve max-depth problem, add bfs solution for invert tree problem

--- tree/binary-tree/invert-tree.js
+++ tree/binary-tree/invert-tree.js
@@ -13,7 +13,7 @@ const { toBinaryTree } = require("./utils");
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = function (root) {
+var dfStackInvertTree = function (root) {
   //      4
   //    /   \
   //   2     7
@@ -29,29 +29,22 @@ var invertTree = function (root) {
 
   // maintain loop in case of not traversing through tree
   while (stack.length || curr) {
-    let node = stack.pop();
+    const node = stack.pop();
 
     if (!curr) {
       newRoot = node;
     }
 
     curr = node;
-    node = null;
+
+    if (curr?.right) stack.push(curr.right);
+    if (curr?.left) stack.push(curr.left);
 
     // perform invert nodes
-    if (curr && curr.right) {
-      stack.push(curr.right);
-
-      if (curr.left) stack.push(curr.left);
-
-      const temp = curr.right;
-      curr.right = curr.left;
+    if (curr && (curr?.left || curr?.right)) {
+      const temp = curr?.right;
+      curr.right = curr?.left;
       curr.left = temp;
-    } else if (curr && curr.left) {
-      // no right, just left
-      stack.push(curr.left);
-      curr.right = curr.left;
-      curr.left = null;
     }
   }
 
@@ -61,26 +54,58 @@ var invertTree = function (root) {
 const tree = toBinaryTree([4, 2, 7, 1, 3, 6, 9]);
 console.log(tree);
 
-
-const start = performance.now();
-console.log(invertTree(tree));
-console.log("depth-first stack take: ", performance.now() - start + "ms")
+// const start = performance.now();
+// console.log(dfStackInvertTree(tree));
+// console.log("depth-first stack take: ", performance.now() - start + "ms");
 
 const newStart = performance.now();
-var recursionInvertTree = function(root) {
-    if (!root) {
-        return root;
-    }
 
-    const temp = root.right;
-    root.right = root.left;
-    root.left = temp;
-
-    recursionInvertTree(root.left);
-    recursionInvertTree(root.right);
-
+var recursionInvertTree = function (root) {
+  if (!root) {
     return root;
+  }
+
+  const temp = root.right;
+  root.right = root.left;
+  root.left = temp;
+
+  recursionInvertTree(root.left);
+  recursionInvertTree(root.right);
+
+  return root;
 };
 
-console.log(recursionInvertTree(tree));
-console.log("depth-first recursion take: ", performance.now() - newStart + "ms")
+// console.log(recursionInvertTree(tree));
+// console.log(
+//   "depth-first recursion take: ",
+//   performance.now() - newStart + "ms"
+// );
+
+const bfQueueInvertTree = (root) => {
+  let newRoot = null;
+  let curr = null;
+  const queue = [root];
+
+  while (queue.length || curr) {
+    const node = queue.shift();
+
+    if (!curr) {
+      newRoot = node;
+    }
+    curr = node;
+
+    if (curr?.right) queue.push(curr.right);
+    if (curr?.left) queue.push(curr.left);
+
+    // invert node
+    if (curr && (curr?.right || curr?.left)) {
+      const temp = curr?.right;
+      curr.right = curr?.left;
+      curr.left = temp;
+    }
+  }
+
+  return newRoot;
+};
+
+console.log(bfQueueInvertTree(tree))
