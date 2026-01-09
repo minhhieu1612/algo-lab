4a09e1529a392ec48eb26af746ff0e1e37a1572c
minhhieu1612 <hieunguyenminh561@gmail.com>
2026-01-09 15:05:29 +0700

    standardize understanding about backtracking

--- backtracking/notes.js
+++ backtracking/notes.js
@@ -1,4 +1,4 @@
-// backtracking follow this pattern
+// backtracking pattern
 function backtracking() {
   // 1, return when result found to terminate all the recursion stack
   if (foundResut) return;
@@ -6,10 +6,16 @@ function backtracking() {
   // 2, (optional) return when current instance is not eligible
   if (isNotEligible) return;
 
-  // 3, perform action with current instance and move to the next instance(step 2 can be eliminated here)
-  performAction(currentInstance);
-  backtracking(nextInstance);
+  // 3, perform action with current instance
+  doAction(currentInstance);
 
-  // 4, redo(backtrack) the wrong course of steps
-  redo(currentInstance);
+  // 4, move to the next instance(step 2 can be eliminated here)
+  // yes means can move to/ no means skip the next instance
+  if (yes) backtracking(nextInstance);
+
+  // 5, undo(backtrack) with the current instance
+  undoAction(currentInstance);
 }
+
+// Notes: instance can be a range or an enumeration or can process next
+// multiple instances
